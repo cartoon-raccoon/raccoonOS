79902fd8f345da4958542b9bc2270f8b9ffd66a7
cartoon-raccoon <shaughn.chan@gmail.com>
2020-12-30 12:14:38 +0800

    Build scripts

---
+++ iso.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+. ./build.sh
+
+mkdir -p isodir
+mkdir -p isodir/boot
+mkdir -p isodir/boot/grub
+
+cp sysroot/boot/raccoonos.kernel isodir/boot/raccoonos.kernel
+cat > isodir/boot/grub/grub.cfg << EOF
+menuentry "raccoonos" {
+    multiboot /boot/raccoonos.kernel
+}
+EOF
+grub-mkrescue -o raccoonos.iso isodir
