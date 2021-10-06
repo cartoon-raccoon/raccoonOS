29ae5f6fb0029f2afb141114430287e992cbe6f4
cartoon-raccoon <shaughn.chan@gmail.com>
2021-10-06 14:16:24 +0800

    Converted boot.S to nasm syntax

---
+++ kernel/arch/i386/boot.asm
@@ -0,0 +1,34 @@
+ALIGNMENT equ 1<<0
+MEMINFO   equ 1<<1
+FLAGS     equ ALIGNMENT | MEMINFO
+MAGIC     equ 1badb002h
+CHECKSUM  equ -(MAGIC + FLAGS)
+ 
+section .multiboot
+align 4
+dd MAGIC
+dd FLAGS
+dd CHECKSUM
+ 
+section .bss
+align 16
+stack_bottom:
+resb 16384
+stack_top:
+ 
+[bits 32]
+section .text
+global _start
+ 
+extern _init
+extern kernel_main
+ 
+_start:
+    mov esp, stack_top
+ 
+    call _init
+ 
+    call kernel_main
+    cli
+.1: hlt
+    jmp .1
