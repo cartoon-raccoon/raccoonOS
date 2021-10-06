fa8f38cd6791489a4ad98ddd49e912f2d40ed0e6
cartoon-raccoon <shaughn.chan@gmail.com>
2021-10-06 18:01:42 +0800

    Commented out buggy code

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <kernel/tty.h>
-#include <kernel/desc_tables.h>
+//#include <kernel/desc_tables.h>
 
 void kernel_main(void) {
     terminal_init();
-    init_desc_tables();
+    //init_desc_tables();
     printf("Welcome to RaccoonOS\n");
     printf("\n");
-    asm volatile ("int $0x3");
+    //asm volatile ("int $0x3");
     //asm volatile ("int $0x4");
 }
