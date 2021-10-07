8142d8b109b02648694a5065ccdfd2bc36d2a57f
cartoon-raccoon <shaughn.chan@gmail.com>
2021-10-07 17:13:45 +0800

    Added serial to file redirect in qemu options

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,4 +8,6 @@ if [[ $1 == "-d" ]]; then
     DEBUG="-d int -no-reboot"
 fi
 
-qemu-system-$(./scripts/target-triplet-to-arch.sh $HOST) $DEBUG -cdrom raccoonos.iso
+qemu-system-$(./scripts/target-triplet-to-arch.sh $HOST) \
+    -serial file:serial.log \
+    $DEBUG -cdrom raccoonos.iso
