ee2ee82efe33b7e7a4dba792d202a78a95557394
cartoon-raccoon <shaughn.chan@gmail.com>
2021-10-22 21:42:16 +0800

    Formatting for printf

--- libc/stdio/printf.c
+++ libc/stdio/printf.c
@@ -4,7 +4,8 @@
 #include <stdio.h>
 #include <string.h>
 
-static bool print(const char* data, size_t len) {
+static bool print(const char* data, size_t len)
+{
     const unsigned char* bytes = (const unsigned char*) data;
     for (size_t i = 0; i < len; i++)
         if (putchar(bytes[i]) == EOF)
@@ -12,22 +13,26 @@ static bool print(const char* data, size_t len) {
     return true;
 }
 
-int printf(const char* restrict format, ...) {
+int printf(const char* restrict format, ...)
+{
     va_list parameters;
     va_start(parameters, format);
 
     int written = 0;
 
-    while (*format != '\0') {
+    while (*format != '\0')
+    {
         size_t maxrem = INT_MAX - written;
 
-        if (format[0] != '%' || format[1] == '%') {
+        if (format[0] != '%' || format[1] == '%')
+        {
             if (format[0] == '%')
                 format++;
             size_t amount = 1;
             while (format[amount] && format[amount] != '%')
                 amount++;
-            if (maxrem < amount) {
+            if (maxrem < amount)
+            {
                 //todo: set errno to overflow
                 return -1;
             }
@@ -40,17 +45,21 @@ int printf(const char* restrict format, ...) {
 
         const char* format_begun_at = format++;
 
-        if (*format == 'c') {
+        if (*format == 'c')
+        {
             format++;
             char c = (char) va_arg(parameters, int);
-            if (!maxrem) {
+            if (!maxrem)
+            {
                 //todo: set errno to overflow
                 return -1;
             }
             if (!print(&c, sizeof(c)))
                 return -1;
             written++;
-        } else if (*format == 's') {
+        } 
+        else if (*format == 's')
+        {
             format++;
             const char* str = va_arg(parameters, const char*);
             size_t len = strlen(str);
@@ -61,7 +70,14 @@ int printf(const char* restrict format, ...) {
             if (!print(str, len))
                 return -1;
             written += len;
-        } else {
+        } 
+        // else if (*format == 'b')
+        // {
+        //     format++;
+        //     const char* str
+        // }
+        else 
+        {
             format = format_begun_at;
             size_t len = strlen(format);
             if (maxrem < len) {
